454c479dd7dc22b3c279337dba0908d8d1d3c710
Amanda <amanda.augusta.prestes@escola.pr.gov.br>
2025-09-04 10:57:58 -0300

    ok

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 const reiniciarBtn = document.getElementById('btn-reiniciar')
 
-avanca.forEach(button =>
- button.addEventListener('clik')
-)
+avanca.forEach(button =>{
+ button.addEventListener('clik',function(){
+    const atual = document.querySelector('.ativo');
+    const proximoPasso = 'passo-'+this.getAtribute('data-proximo');
+
+    atual.classList.remove('ativo');
+    cont proximoElemento= document.getElementById(proximoPasso);
+
+    if(proximoElemento){
+        proximoElemento.classList.add('ativo');
+    } else {
+            console.error(`Elemento com ID "${proximoPasso}" não encontrado.`)
+    }
+  });
+});
