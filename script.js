ba7b3399f5c9de507d5b0dc9dfd73f30476c4b1e
Amanda <amanda.augusta.prestes@escola.pr.gov.br>
2025-09-11 11:01:06 -0300

    ok

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ avanca.forEach(button =>{
  button.addEventListener('clik',function(){
     const atual = document.querySelector('.ativo');
     const proximoPasso = 'passo-'+this.getAtribute('data-proximo');
-    
+
     atual.classList.remove('ativo');
     const proximoElemento= document.getElementById(proximoPasso);
 
